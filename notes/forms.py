016b9327b426705f5a5ed56d0c53743a47c9cace
root <root@DESKTOP-B8VV4GM.localdomain>
2022-05-09 21:39:16 +0200

    added basic input validation

--- notes/forms.py
+++ notes/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.core import validators
 
 
 class NoteForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea,
-                           label='Text Note', max_length=300)
+    text = forms.CharField(
+        validators=[validators.MinLengthValidator(
+            2, "Please enter a note at least 2 characters long.")],
+        widget=forms.Textarea,
+        label='Text Note', max_length=300)
